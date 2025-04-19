35d7b0c0cf36296fc52f5179e5df868832bd7af7
Shawnm-cyber <shawnmathew14@gmail.com>
2025-04-18 23:48:54 -0400

    Fixed message_boards_page.dart and main.dart for all forms of navigation for app

--- lib/home/message_boards_page.dart
+++ lib/home/message_boards_page.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:message_board/home/chat_page.dart';
+import 'chat_page.dart'; // Ensure this page exists
 
 class MessageBoardsPage extends StatelessWidget {
   @override
